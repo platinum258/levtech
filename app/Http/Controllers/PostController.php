68fe560a88c9cc307774fe2684fc4bd2a59f37c2
platinum258 <szq1786340401@icloud.com>
2023-12-07 16:01:17 +0900

    after 08-6

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
-use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
 
 class PostController extends Controller
 {
@@ -11,6 +11,37 @@ class PostController extends Controller
     {
         return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
        //blade内で使う変数'posts'と設定。'posts'の中身にgetを使い、インスタンス化した$postを代入。
+    }
+    public function show(Post $post)
+    {
+        return view('posts.show')->with(['post' => $post]);
+       //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
+    }
+    public function create()
+    {
+    return view('posts.create');
+    }
+    public function store(Post $post, PostRequest $request)
+    {
+       $input = $request['post'];
+       $post->fill($input)->save();
+       return redirect('/posts/' . $post->id);
+    }
+    public function edit(Post $post)
+    {
+        return view('posts.edit')->with(['post' => $post]);
+    }
+    public function update(PostRequest $request, Post $post)
+    {
+        $input_post = $request['post'];
+        $post->fill($input_post)->save();
+
+        return redirect('/posts/' . $post->id);
+    }
+    public function delete(Post $post)
+    {
+        $post->delete();
+        return redirect('/');
     }
 }
 ?>
