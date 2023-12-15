6e1e323c26675a14a9cff662ca8202fdcb6178cf
platinum258 <szq1786340401@icloud.com>
2023-12-15 20:33:56 +0900

    after 09_4

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -30,6 +30,16 @@
                 </div>
             @endforeach
         </div>
+        
+        <div>
+        @foreach($questions as $question)
+            <div>
+                <a href="https://teratail.com/questions/{{ $question['id'] }}">
+                {{ $question['title'] }}
+                </a>  
+            </div>
+        @endforeach
+        </div>
         <div class='paginate'>
             {{ $posts->links() }}
         </div>
